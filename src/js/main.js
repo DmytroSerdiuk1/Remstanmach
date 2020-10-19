e58cd91839634978fc8da78cd1422b88d1d17587
Nomid <dimongeimer2002@gmail.com>
2020-10-20 01:14:32 +0300

    add mobile adaptability Fix

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,23 @@
 import Swiper, { Navigation, Pagination } from 'swiper';
 Swiper.use([Navigation, Pagination]);
 new Swiper('.supply-slider', {
-    slidesPerView: 4,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+
+    breakpoints: {
+        425: {
+          slidesPerView: 1,
+          loop: true,
+          navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+            },
+        },
+        1000: {
+            slidesPerView: 4,
+            loop: true,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        }
+      }
 })
